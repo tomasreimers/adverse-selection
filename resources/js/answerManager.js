e9f402f70ca1dc8d7e43deeb2ff0e744bb10e79d
Jeff Gortmaker <jebego@gmail.com>
2012-05-26 22:24:39 -0400

    added laffter curve answer

--- resources/js/answerManager.js
+++ resources/js/answerManager.js
@@ -2,7 +2,7 @@ var answers = new Array(); // stores answers
 
 function loadAnswers(callback){
     // TODO: Load XML Files here
-    answers = ["Keynes", "Demand curve shifts left", "Demand curve shifts right", "Horizontal"];
+    answers = ["Keynes", "Laffer curve", "Demand curve shifts right", "Horizontal"];
     // activate callback when done
     callback("answers");
 }

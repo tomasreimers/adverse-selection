e13a32678458528cf6d686799e4d747aed13ae2a
Tomas <tomasreimers@gmail.com>
2012-05-29 15:09:34 -0300

    Update resources/js/answerManager.js

--- resources/js/answerManager.js
+++ resources/js/answerManager.js
@@ -2,7 +2,7 @@ var answers = new Array(); // stores answers
 
 function loadAnswers(callback){
     // TODO: Load XML Files here
-    answers = ["Keynes", "Laffer curve", "Demand curve shifts right", "Horizontal"];
+    answers = ["Keynes", "Laffer curve", "Demand curve shifts right", "Horizontal", "Demand curve shifts left"];
     callback("answers");
 }
 

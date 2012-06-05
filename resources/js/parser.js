3e4bcd8042c439ff9a60577c62cc510d0e0995e9
Jeff Gortmaker <jebego@gmail.com>
2012-06-05 18:54:28 -0400

    modified parsing format to be txt-based

--- resources/js/parser.js
+++ resources/js/parser.js
@@ -1,27 +1,35 @@
 function parseQuestions(txtData){
-	var lines = txtData.split('\n');
+	var lines = txtData.replace(/(\r)/gm, "").split('\n');
 	var questions = new Array();
 	var currentSection = new Array();
 	for (var i = 0; i < lines.length; i ++){
 		// check for new section
 		if (lines[i][0] == '#'){
-			questions.push(currentSection);
+			// don't add blank section if first line
+			if (i != 0){
+				questions.push(currentSection);
+			}
 			currentSection = []
 			currentSection.push(lines[i].substring(1));
 		}
 		// check for new question
 		else if (lines[i] == '' && lines[i + 2] != ''){
-			var currentQuestion = new Array(lines[i + 1].trim(), lines[i + 2], lines[i + 3].trim());	
+			var currentQuestion = new Array($.trim(lines[i + 1]), lines[i + 2], $.trim(lines[i + 3]));	
 			currentSection.push(currentQuestion);
 			i += 3;
 		}
 	}
+	//push final section and return questions
+	questions.push(currentSection);
 	return questions;
 }
 
 function parseAnswers(txtData){
-	//pass
+	var lines = txtData.replace(/(\r)/gm, "").split('\n');
+	var answers = new Array();
+	// add trimmed lines to answers array
+	for (var i = 0; i < lines.length; i ++){
+		answers.push($.trim(lines[i]));
+	}
+	return answers;
 }
-
-// add fast trim method
-String.prototype.trim = String.prototype.trim || function trim() { return this.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); };
